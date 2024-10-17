c8f6b441ff4c9b10f7780e7e42d5d213eeb3d93b
ysm-dev <i@ysm.dev>
2024-10-18 00:25:59 +0900

    p

--- app/page.tsx
+++ app/page.tsx
@@ -5,72 +5,55 @@ import { bytesToHex } from "@noble/hashes/utils"
 import { useQuery } from "@tanstack/react-query"
 import { useSearchParam } from "hooks/useSearchParam"
 import { storage } from "lib/storage"
+import { useRouter } from "next/navigation"
 import { getIPFSURL } from "src/getIPFSURL"
-import { isServer } from "utils/isServer"
-
-// if (!isServer()) {
-//   ;(async () => {
-//     const [url, timestamp] = location.search.slice(1).split("/")
-
-//     const hash = bytesToHex(sha256(url))
-
-//     console.log(hash)
-
-//     const keys = await storage(true).keys(`${hash}`)
-
-//     const [data, snapshot] = keys.slice(-2)
-
-//     const time = data.split(":")[1]
-
-//     const cid = await storage(false).get<string>(data)
-
-//     if (!cid) {
-//       console.log("no cid found")
-//       return
-//     }
-
-//     if (!timestamp) {
-//       history.replaceState(null, "", `?${url}/${time}`)
-//     }
-
-//     console.log(cid)
-//   })()
-// }
 
 export default function Page() {
   const { url, timestamp } = useSearchParam()
+  const { replace } = useRouter()
 
-  const { data } = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["cid", timestamp, url],
     queryFn: async () => {
       const hash = bytesToHex(sha256(url))
 
       const keys = await storage().keys(`${hash}`)
 
+      if (keys.length === 0) {
+        return null
+      }
+
       const [data, snapshot] = keys.slice(-2)
 
-      console.log(data, snapshot)
+      const cid1 = data.split(":").pop()?.slice(2)
+      const cid2 = snapshot.split(":").pop()?.slice(2)
+      const time = data.split(":").at(-2)
 
-      const [cid1, cid2] = await Promise.all([
-        storage(false).get<string>(data),
-        storage(false).get<string>(snapshot),
-      ])
-
-      // const [{ value: cid1 }, { value: cid2 }] = await storage(
-      //   false,
-      // ).getItems<string>([data, snapshot])
-
-      console.log({ cid1, cid2 })
+      if (time !== timestamp) {
+        replace(`?${url}/${time}`)
+      }
 
       return { cid1, cid2 }
     },
   })
 
-  console.log({ url, timestamp })
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+
+  if (!isPending && !data) {
+    return <div>Not found</div>
+  }
 
   return (
-    <div className="m-2 p-2 text-2xl">
-      <div className="">1</div>
+    <div className="flex h-dvh text-2xl">
+      {data?.cid1 && (
+        <iframe
+          className="mt-16 w-full flex-1"
+          title={`Archive of ${url} at ${timestamp}`}
+          src={getIPFSURL(data.cid1)}
+        />
+      )}
     </div>
   )
 }
